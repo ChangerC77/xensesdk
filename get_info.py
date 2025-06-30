dd9d75f67a17193ace1e1e19512a3c940ec847c3
changerc77 <1216898523@qq.com>
2025-06-30 15:09:03 +0800

    add more information in get_info.py

--- get_info.py
+++ get_info.py
@@ -4,26 +4,27 @@ from xensesdk.xenseInterface.XenseSensor import Sensor
 
 def main(sensor_0):
     
-    force, res_force, mesh_init, src, diff, depth = sensor_0.selectSensorInfo(
-        Sensor.OutputType.Force, 
-        Sensor.OutputType.ForceResultant,
-        Sensor.OutputType.Mesh3DInit,
-        Sensor.OutputType.Rectify, 
-        Sensor.OutputType.Difference, 
-        Sensor.OutputType.Depth,
+    rectify, difference, depth, force, forcenormal, forceresultant, mesh3d, mesh3dinit, mesh3dflow = sensor_0.selectSensorInfo(
+        Sensor.OutputType.Rectify, # [np.ndarray] # 校正图像, shape=(700, 400, 3), RGB
+        Sensor.OutputType.Difference, # [np.ndarray] # 差分图像, shape=(700, 400, 3), RGB
+        Sensor.OutputType.Depth, # [np.ndarray] # 深度图像, shape=(700, 400), 单位mm
+        Sensor.OutputType.Force, # [np.ndarray] # 三维力分布, shape=(35, 20, 3)
+        Sensor.OutputType.ForceNorm, # [np.ndarray] # 法向力分量, shape=(35, 20, 3)
+        Sensor.OutputType.ForceResultant, # [np.ndarray] # 六维合力, shape=(6,)
+        Sensor.OutputType.Mesh3D, # [np.ndarray] # 当前帧3D网格, shape=(35, 20, 3)
+        Sensor.OutputType.Mesh3DInit, # [np.ndarray] # 初始3D网格, shape=(35, 20, 3)
+        Sensor.OutputType.Mesh3DFlow, # [np.ndarray] # 网格形变向量, shape=(35, 20, 3)
     )
-    print("force", force) # (35, 20, 3)
-    print("res_force", res_force) # (6,)
-    print("mesh_init", mesh_init) # (35, 20, 3)
-    print("src", src) # (700, 400, 3)
-    print("diff", diff) # (700, 400, 3)
-    print("depth", depth) # (700, 400)
-    # print("force", force.shape)
-    # print("res_force", res_force.shape)
-    # print("mesh_init", mesh_init.shape)
-    # print("src", src.shape)
-    # print("diff", diff.shape)
-    # print("depth", depth.shape)
+    print("rectify", rectify)
+    print("difference", difference)
+    print("depth", depth)
+    print("force", force)
+    print("forcenormal", forcenormal)
+    print("forceresultant", forceresultant)
+    print("rectify", rectify)
+    print("mesh3d", mesh3d)
+    print("mesh3dinit", mesh3dinit)
+    print("mesh3dflow", mesh3dflow)
 
     sensor_0.release()
     sys.exit()
